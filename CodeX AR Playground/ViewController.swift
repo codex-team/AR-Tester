6276e18803c369bdbeffdc60b1ce6c446345c78a
N0str <ntpcp@yandex.ru>
2017-09-01 18:20:37 +0300

    Add touch Event

--- CodeX AR Playground/ViewController.swift
+++ CodeX AR Playground/ViewController.swift
@@ -22,16 +22,54 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.debugOptions  = [.showConstraints, .showLightExtents, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        sceneView.automaticallyUpdatesLighting = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/iPad.scn")!
-        
-        let childs = scene.rootNode.childNodes
-        
-        debugPrint(childs)
-        
+        let scene = SCNScene()
         // Set the scene to the view
         sceneView.scene = scene
+    }
+    
+    func addNewShip(position: SCNVector3) {
+        
+        let sceneIpad = SCNScene(named: "art.scnassets/iPad.scn")
+        
+        let planeNode = SCNNode(geometry: sceneIpad?.rootNode.childNode(withName: "iPad", recursively: false)?.geometry)
+        
+        planeNode.scale = SCNVector3Make(0.001, 0.001, 0.001);
+        planeNode.eulerAngles.y = sceneView.pointOfView!.eulerAngles.y
+        planeNode.eulerAngles.x = sceneView.pointOfView!.eulerAngles.x
+        planeNode.eulerAngles.z = sceneView.pointOfView!.eulerAngles.z
+        planeNode.position = SCNVector3Make(position.x, position.y, position.z)
+        
+        sceneView.scene.rootNode.addChildNode(planeNode)
+        
+//        let planeNode = SCNNode(geometry: textNode)
+//        planeNode.scale = SCNVector3Make(0.05, 0.05, 0.05);
+//        planeNode.eulerAngles.y = sceneView.pointOfView!.eulerAngles.y
+//        planeNode.eulerAngles.x = sceneView.pointOfView!.eulerAngles.x
+//        planeNode.eulerAngles.z = sceneView.pointOfView!.eulerAngles.z
+        
+        sceneView.scene.rootNode.addChildNode(planeNode)
+    }
+    
+    func floatBetween(_ first: Float,  and second: Float) -> Float { // random float between upper and lower bound (inclusive)
+        return (Float(arc4random()) / Float(UInt32.max)) * (first - second) + second
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        debugPrint("touchesBegan")
+        
+        guard let touch = touches.first else { return }
+        let results = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        guard let hitFeature = results.last else { return }
+        let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+        let hitPosition = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+        
+        addNewShip(position: hitPosition)
     }
     
     override func viewWillAppear(_ animated: Bool) {
