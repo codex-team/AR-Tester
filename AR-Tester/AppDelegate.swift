1b662b10a59346a64a8ddc28d256bfa4ec78fbe1
Egor Petrov <petrov.bz.molod@gmail.com>
2017-10-14 12:31:46 +0300

    Fix AppDelegate

--- AR-Tester/AppDelegate.swift
+++ AR-Tester/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-		func application(_ application: UIApplication,
-		                 didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-      Fabric.with([Crashlytics.self])
-			return true
-		}
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
+        return true
+    }
 }
